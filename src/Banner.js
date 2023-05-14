5807ec9166b6afc6ab2add3d538b7f32207ea299
William Wong <wonghinfung0325@gmail.com>
2023-05-13 21:09:16 -0400

    fetch data using api

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+  const [movie, setMovie] = React.useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
   return (
     <header
       className="banner"
@@ -17,7 +40,9 @@ function Banner() {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">This is a test description</h1>
+        <h1 className="banner__description">
+          {truncate(`This is a test description`, 150)}
+        </h1>
       </div>
 
       <div className="banner--fadeBottom" />
