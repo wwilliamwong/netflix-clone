d61182d49c43ba8829bb384bf65d60e6dc9d49fa
William Wong <wonghinfung0325@gmail.com>
2023-05-17 09:52:33 -0400

    added style to signin page

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,13 +2,30 @@ import React from "react";
 import "./SignupScreen.css";
 
 function SignupScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="Email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button type="submit">Sign In</button>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+
+        <h4>
+          <span className="signupScreen__gray">New to Netflix?</span>{" "}
+          <span className="signupScreen__link" onClick={register}>
+            Sign Up Now
+          </span>
+        </h4>
       </form>
     </div>
   );
