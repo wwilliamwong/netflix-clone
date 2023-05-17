72b7d3d34ab170d327a0f3c8e015ac4bbbbafbea
William Wong <wonghinfung0325@gmail.com>
2023-05-17 17:14:43 -0400

    added signin and signup feature

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,8 @@
 import React, { useRef } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../firebase";
 import "./SignupScreen.css";
 
@@ -24,6 +27,18 @@ function SignupScreen() {
 
   const signIn = (e) => {
     e.preventDefault();
+
+    signInWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
