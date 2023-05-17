5d1b44546fa42bb069c29d4aded6c6ddda365a63
William Wong <wonghinfung0325@gmail.com>
2023-05-17 10:57:24 -0400

    setup register with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBWMXtA1z-DKPC1hAZHISEqx8BUueyIudM",
   authDomain: "netflix-clone-83631.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "557400341337",
   appId: "1:557400341337:web:9b7bbfc2ea283f1a5eaa48",
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth };
+export default db;
