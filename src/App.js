8c392d92be5455aaa292e96036d65c2e05d016a4
William Wong <wonghinfung0325@gmail.com>
2023-05-17 17:27:28 -0400

    fixed minor bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "./firebase";
 
 export default function App() {
   const user = null;
