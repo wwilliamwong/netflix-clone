f65e42279a515cb16d986acca57c3f5d1edef2bb
William Wong <wonghinfung0325@gmail.com>
2023-05-17 17:26:22 -0400

    implemented login & logout logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function App() {
   const user = null;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        //Logged in
+      } else {
+        //Logged out
+      }
+    });
+  }, []);
 
   return (
     <div className="app">
